13533df50853b05320fe0c6700646982e7c31a72
ArturoSbr <soberonarturo@gmail.com>
2022-05-13 08:07:34 -0500

    Scraper iterates until end of section

--- cod/01-scrape-comments/comments.py
+++ cod/01-scrape-comments/comments.py
@@ -1,47 +1,72 @@
-# Libraries
-import os
-from dotenv import load_dotenv
+# Import libraries
+from googleapiclient.discovery import build
 
-# Load api key from .env
-load_dotenv()
-key = os.getenv('API_KEY')
+# Function to scrape comments
+def getComments(apiKey=None, videoId=None):
+    
+    # Build youtube instance
+    youtube = build(serviceName='youtube', version='v3', developerKey=apiKey)
 
-# Test
-if key is not None:
-    print('\nWOW it worked!!')
-# -*- coding: utf-8 -*-
+    # Build fields
+    token = 'nextPageToken'
+    author = 'items(snippet/topLevelComment/snippet/authorDisplayName)'
+    comment = 'items(snippet/topLevelComment/snippet/textOriginal)'
+    date = 'items(snippet/topLevelComment/snippet/publishedAt)'
 
-# Sample Python code for youtube.commentThreads.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
+    # Declare request
+    req = youtube.commentThreads().list(
+        part='snippet',
+        moderationStatus='published',
+        order='time',
+        textFormat='plainText',
+        videoId=videoId,
+        fields=', '.join([token, author, comment, date]),
+        prettyPrint=True,
+        maxResults=50
+    )
 
+    # Send request
+    res = req.execute()
 
-# import googleapiclient.discovery
+    # Extract comments
+    comments = res['items']
 
-# def main():
-#     # Disable OAuthlib's HTTPS verification when running locally.
-#     # *DO NOT* leave this option enabled in production.
-#     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    # Store nextPageToken
+    try:
+        npt = res['nextPageToken']
+    except:
+        npt = None
 
-#     api_service_name = "youtube"
-#     api_version = "v3"
-#     key = 
+    # Iterate through subsequent pages
+    while npt is not None:
+        # Buid another request
+        req = youtube.commentThreads().list(
+            part='snippet',
+            moderationStatus='published',
+            order='time',
+            textFormat='plainText',
+            videoId=videoId,
+            fields=', '.join([token, author, comment, date]),
+            prettyPrint=True,
+            maxResults=15,
+            pageToken=npt
+        )
 
-#     youtube = googleapiclient.discovery.build(
-#         api_service_name, api_version, developerKey = DEVELOPER_KEY)
+        # Send request
+        res = req.execute()
+        
+        # Update nextPageToken
+        try:
+            npt = res['nextPageToken']
+        except:
+            npt = None
 
-#     request = youtube.commentThreads().list(
-#         part="snippet",
-#         moderationStatus="published",
-#         order="time",
-#         textFormat="plainText",
-#         videoId="ASzOzrB-a9E",
-#         fields="nextPageToken, items(snippet/topLevelComment/snippet)",
-#         prettyPrint=True
-#     )
-#     response = request.execute()
+        # Add new comments
+        comments = comments.append(res['items'])
 
-#     print(response)
+    # Return response
+    return comments
 
-# if __name__ == "__main__":
-#     main()
+# Execute program
+if __name__ == '__main__':
+    main()
